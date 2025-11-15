deca260b136e32b3029dbb511666b48350cb6b8c
azb <azbaney@gmail.com>
2025-11-15 15:05:44 -0800

    Make snipping use a collider instead of distance

--- Assets/WireClippingMechanic/Wire.cs
+++ Assets/WireClippingMechanic/Wire.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using OVR;
 using UnityEngine.Events;
 
 public class Wire : MonoBehaviour
@@ -7,73 +6,47 @@ public class Wire : MonoBehaviour
     public GameObject NonCutWireModel;
     public GameObject CutWireModel;
 
-    public OVRHand LeftHand, RightHand;
-
     public bool Snipped = false;
 
     public UnityEvent OnSnipEvent;
 
     AudioSource audioSource;
 
-    bool LeftHandPinching;
-    bool RightHandPinching;
+    Collider WireCollider;
+
+    public Transform grabbable;
+
+    Vector3 startPosition;
 
     void Start()
     {
         NonCutWireModel.SetActive(true);
         CutWireModel.SetActive(false);
         audioSource = GetComponent<AudioSource>();
+        WireCollider = GetComponent<BoxCollider>();
+        startPosition = grabbable.transform.position;
     }
 
     void Update()
     {
-        bool prevLeftHandPinching = LeftHandPinching;
-        bool prevRightHandPinching = RightHandPinching;
-
-        LeftHandPinching = LeftHand.GetFingerIsPinching(OVRHand.HandFinger.Index);
-        RightHandPinching = RightHand.GetFingerIsPinching(OVRHand.HandFinger.Index);
-
-        bool LeftPinchPressed = LeftHandPinching && !prevLeftHandPinching;
-        bool LeftPinchReleased = !LeftHandPinching && prevLeftHandPinching;
-
-        bool RightPinchPressed = RightHandPinching && !prevRightHandPinching;
-        bool RightPinchReleased = !RightHandPinching && prevRightHandPinching;
-        Vector3 LeftHandPinchPosition = LeftHand.PointerPose.position;
-        Vector3 RightHandPinchPosition = RightHand.PointerPose.position;
-
-        if (LeftPinchPressed)
+        if (grabbable.transform.position != startPosition)
         {
-            if (audioSource != null && audioSource.clip != null)
-            {
-                audioSource.PlayOneShot(audioSource.clip);
-            }
-            Debug.Log("LeftHandPinching");
-        }
-        if (RightPinchPressed)
-        {
-            if (audioSource != null && audioSource.clip != null)
-            {
-                audioSource.PlayOneShot(audioSource.clip);
-            }
-            Debug.Log("RightHandPinching");
-        }
-        if (!Snipped)
-        {
-            CutWireWithPinch(LeftHandPinchPosition, LeftPinchPressed);
-            CutWireWithPinch(RightHandPinchPosition, RightPinchPressed);
+            CutWireWithPinch();
+            Destroy(grabbable.gameObject);
         }
     }
 
-    void CutWireWithPinch(Vector3 pinchPosition, bool isPinching)
+    void CutWireWithPinch()
     {
-        if (isPinching && !Snipped)
+        if (!Snipped)
         {
-            if (Vector3.Distance(pinchPosition, transform.position) < .05f)
+            NonCutWireModel.SetActive(false);
+            CutWireModel.SetActive(true);
+            Snipped = true;
+            OnSnipEvent.Invoke();
+            if (audioSource != null && audioSource.clip != null)
             {
-                NonCutWireModel.SetActive(false);
-                CutWireModel.SetActive(true);
-                Snipped = true;
-                OnSnipEvent.Invoke();
+                audioSource.PlayOneShot(audioSource.clip);
             }
         }
     }
